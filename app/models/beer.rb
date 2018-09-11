96164e1deff008d6bbe683cabba6c3b146220b89
puhuri666 <pmarkkola@gmail.com>
2018-09-11 10:33:12 +0300

    t9

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,13 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    "#{ratings.average(:score)}"
+  end
+
+  def to_s
+    "#{self.name} (#{self.brewery.name})"
+  end
+
 end
