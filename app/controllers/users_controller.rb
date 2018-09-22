91a9daa105d1df4a5eb50802c609b89b529c612c
puhuri666 <pmarkkola@gmail.com>
2018-09-22 12:11:19 +0300

    rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   def update
     if @user == current_user
       respond_to do |format|
-        if user_params[:username].nil? and @user == current_user && @user.update(user_params)
+        if user_params[:username].nil? && @user == current_user && @user.update(user_params)
           format.html { redirect_to @user, notice: 'User was successfully updated.' }
           format.json { head :no_content }
         else
