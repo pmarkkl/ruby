c6952db471c39928d2eda6d13c8094ffc1ab608e
puhuri666 <pmarkkola@gmail.com>
2018-10-07 22:35:01 +0300

    välicommit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,6 +9,7 @@ class PlacesController < ApplicationController
 
   def search
     @places = BeermappingApi.places_in(params[:city])
+    Rails.cache.write('city', params[:city])
     if @places.empty?
       redirect_to places_path, notice: "No locations in #{params[:city]}"
     else
